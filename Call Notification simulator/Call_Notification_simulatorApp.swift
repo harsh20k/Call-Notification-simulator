37c615c53d223b9404d2cd08e5b088cdfbd68d93
harsh <bighugscafe@gmail.com>
2025-04-17 17:58:43 +0530

    Initial Commit

---
+++ Call Notification simulator/Call_Notification_simulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Call_Notification_simulatorApp.swift
+//  Call Notification simulator
+//
+//  Created by harsh  on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Call_Notification_simulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
